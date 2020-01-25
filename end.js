437de90d68330d67e30683903d2e85b655b06dfc
Sam <sam.eskandar00@hotmail.com>
2020-01-25 10:09:23 -0500

    add highscores

--- end.js
+++ end.js
@@ -4,6 +4,11 @@ const mostRecentScore = localStorage.getItem("mostRecentScore");
 const finalScore = document.getElementById('finalScore');
 finalScore.innerText = mostRecentScore;
 
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+
+const MAX_HIGH_SCORES = 5;
+
 username.addEventListener('keyup', () => {
 	console.log(username.value);
 	saveScoreBtn.disabled = !username.value;
@@ -12,4 +17,18 @@ username.addEventListener('keyup', () => {
 saveHighScore = e => {
 	console.log("clicked the save button!");
 	e.preventDefault();
+
+	const score = {
+		score: Math.floor(Math.random() * 100),
+		name: username.value,
+	};
+	highScores.push(score);
+	highScores.sort( (a, b) => {
+		return b.score - a.score;
+	});
+
+	highScores.splice(MAX_HIGH_SCORES);
+
+	localStorage.setItem("highScores", JSON.stringify(highScores));
+	window.location.assign("index.html");
 }
