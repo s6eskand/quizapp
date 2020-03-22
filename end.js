f05576b6421cfb712b61118bc5c81fb24b5b7216
Sam <sam.eskandar00@hotmail.com>
2020-03-22 16:18:27 -0400

    fix logic issues

--- end.js
+++ end.js
@@ -1,16 +1,15 @@
-const username = document.getElementById('username');
-const saveScoreBtn = document.getElementById('saveScoreBtn');
+const username = document.getElementById("username");
+const saveScoreBtn = document.getElementById("saveScoreBtn");
+const finalScore = document.getElementById("finalScore");
 const mostRecentScore = localStorage.getItem("mostRecentScore");
-const finalScore = document.getElementById('finalScore');
-finalScore.innerText = mostRecentScore;
 
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
-
 const MAX_HIGH_SCORES = 5;
 
+finalScore.innerText = mostRecentScore;
+
 username.addEventListener('keyup', () => {
-	console.log(username.value);
 	saveScoreBtn.disabled = !username.value;
 });
 
@@ -23,12 +22,10 @@ saveHighScore = e => {
 		name: username.value,
 	};
 	highScores.push(score);
-	highScores.sort( (a, b) => {
-		return b.score - a.score;
-	});
+	highScores.sort( (a, b) => b.score - a.score);
 
 	highScores.splice(MAX_HIGH_SCORES);
 
 	localStorage.setItem("highScores", JSON.stringify(highScores));
 	window.location.assign("index.html");
-}
+};
